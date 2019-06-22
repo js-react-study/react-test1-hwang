f92a0fe80ae0c884e71dacaeef684091f19931b1
황금중 <gld2000@nate.com>
2019-06-22 19:17:13 +0900

    chap5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'; 
 import './App.css';
+ 
+const mandatas = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '이영한',
+  'birthday': '860403',
+  'gender': '남자',
+  'job': '프로그래머'
+},{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '김미정',
+  'birthday': '540403',
+  'gender': '여자',
+  'job': '디자이너'
+},{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '정우리',
+  'birthday': '850403',
+  'gender': '남자',
+  'job': '청소부'
+}]  
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>고객관리 시스템을 만들어 보자.</h2>
-    </div>
+    <div>
+      {
+        mandatas.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+        })
+
+      }
+    </div>    
   );
 }
 
