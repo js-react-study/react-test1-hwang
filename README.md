8254cbad94e589693ab86e244632493957f4de48
황금중 <gld2000@naver.com>
2019-06-22 17:38:15 +0900

    Vs code - Git test

---
+++ README.md
@@ -0,0 +1,2 @@
+## React lecture learn-1
+리액트 스터디 동빈나 강의 연습
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>고객관리 시스템을 만들어 보자.</h2>
+    </div>
+  );
+}
+
+export default App;
