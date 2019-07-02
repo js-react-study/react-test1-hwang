f95da9c9dfaa537e70cc3cae3fdedd9945e17049
gld2000 <gld2000@nate.com>
2019-07-02 09:38:45 +0900

    Add get Customers REST API

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Customer from './components/Customer'; 
 import './App.css';
 import Paper from '@material-ui/core/Paper';
@@ -20,51 +20,70 @@ const styles = theme => ({
   }
 })
 
-const mandatas = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '이영한',
-  'birthday': '860403',
-  'gender': '남자',
-  'job': '프로그래머'
-},{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '김미정',
-  'birthday': '740403',
-  'gender': '여자',
-  'job': '디자이너'
-},{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '정우리',
-  'birthday': '850403',
-  'gender': '남자',
-  'job': '청소부'
-}]  
+// const mandatas = [{
+//   'id': 1,
+//   'image': 'https://placeimg.com/64/64/1',
+//   'name': '이영한',
+//   'birthday': '860403',
+//   'gender': '남자',
+//   'job': '프로그래머'
+// },{
+//   'id': 2,
+//   'image': 'https://placeimg.com/64/64/2',
+//   'name': '김미정',
+//   'birthday': '740403',
+//   'gender': '여자',
+//   'job': '디자이너'
+// },{
+//   'id': 3,
+//   'image': 'https://placeimg.com/64/64/3',
+//   'name': '정우리',
+//   'birthday': '850403',
+//   'gender': '남자',
+//   'job': '청소부'
+// }]  
 
+class App extends Component {
+  state = {
+    customers: ""
+  }
 
-function App(props) {
-  const { classes } = props;
-  return (     
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {mandatas.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>) })}
-        </TableBody>
-      </Table> 
-    </Paper>  
-  );
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
+
+    const { classes } = this.props;
+    return (     
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>) 
+            }) : ""}
+          </TableBody>
+        </Table> 
+      </Paper>  
+    );
+  }
 }
 
 export default withStyles(styles)(App);
